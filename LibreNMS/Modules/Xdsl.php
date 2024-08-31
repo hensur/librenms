00229a08348b07f3d062ec3d468c648a47142a4b
Henning Surmeier <me@hensur.de>
2024-08-31 13:29:02 +0200

    add vigor 167 dsl ifindex workaround

--- LibreNMS/Modules/Xdsl.php
+++ LibreNMS/Modules/Xdsl.php
@@ -135,6 +135,13 @@ class Xdsl implements Module
         $adsl = \SnmpQuery::hideMib()->walk('ADSL-LINE-MIB::adslMibObjects')->table(1);
         $adslPorts = new Collection;
 
+        if (isset($adsl[0])) {
+            // workaround vigor 167 dsl ifindex
+            $port = (int) $os->getDevice()->ports()->where('ifName', "ptm0")->value('ifIndex');
+            $adsl[$port] = $adsl[0];
+            unset($adsl[0]);
+        }
+
         foreach ($adsl as $ifIndex => $data) {
             // Values are 1/10
             foreach ($this->adslTenthValues as $oid) {
